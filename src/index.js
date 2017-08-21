78e2aba1d5b959386133a44c1c210bcc4d482b7e
Gerben Neven <gerbyzation@me.com>
2017-08-21 10:06:19 +0100

    add form stuff

--- src/index.js
+++ src/index.js
@@ -4,17 +4,40 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     }
   }
 
   componentDidMount() {
     this.getUsers();
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then(res => {
+      this.getUsers();
+      this.setState({ username: '', email: '' });
+    })
+    .catch(err => console.log(err));
   }
 
   getUsers() {
@@ -31,7 +54,14 @@ class App extends Component {
             <br />
             <h1>All Users</h1>
             <hr /><br />
-            <UsersList users={this.state.users} />
+            <AddUser 
+              username={this.state.username}
+              email={this.state.email}
+              addUser={this.addUser.bind(this)}
+              handleChange={this.handleChange.bind(this)}
+            />
+            <br />
+            <UsersList  users={this.state.users} />
           </div>
         </div>
       </div>
